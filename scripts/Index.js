13e1219239dffbad2ef108a6db2fac8d23ef423a
adrians <adiskir@gmail.com>
2024-10-18 19:07:50 +0300

    Normalized design for support and about page. Deleted homepage n watermap.

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,1 +1,25 @@
+let root = document.querySelector(':root');
 
+// Dropdown
+// let dropdownBlock = document.querySelector('dropdown-block'); // Stuff that holds all childrennnn
+// // let dropdownBlock = document.getElementsByTagName('dropdown-block');
+// // dropdownBlock.focus();
+// document.querySelectorAll(".dropdown").forEach(dropdown => {
+//     let rect = dropdown.getBoundingClientRect();
+//     console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+    
+   
+
+//     dropdown.addEventListener("focusin", () => {
+//         dropdownBlock.style.display = 'flex';
+//         dropdownBlock.style.width = (rect.right - rect.left) + 'px';
+//         dropdownBlock.style.left = rect.left + 'px';
+//         dropdownBlock.style.top = rect.bottom + 'px';
+//     });
+
+//     dropdown.addEventListener("focusout", () => {
+//         dropdownBlock.style.display = 'none';
+//         dropdownBlock.style.width = (20) + 'px';
+//     });
+// });
